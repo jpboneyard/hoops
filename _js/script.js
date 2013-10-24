e6e012b47ea15135cf0680af17593ff6cb863c59
JPB <jpboneyard@gmail.com>
2013-10-24 00:45:13 -0400

    scroll to enabled, about section set up. new type and button styles added

--- _js/script.js
+++ _js/script.js
@@ -1,8 +1,28 @@
-/* Author: 
+$(document).ready(function() {
+        $("a.anchorLink").anchorAnimate()
 
-*/
+var scroll_pos = 0;
+var scroll_time;
+
+$(window).scroll(function() {
+    clearTimeout(scroll_time);
+    var current_scroll = $(window).scrollTop();
+    
+    if (current_scroll >= $('#topNav').outerHeight()) {
+        if (current_scroll <= scroll_pos) {
+            $('#topNav').removeClass('hidden');    
+        }
+        else {
+            $('#topNav').addClass('hidden');  
+        }
+    }
+    scroll_time = setTimeout(function() {
+        scroll_pos = $(window).scrollTop();
+    }, 25);
+});
 
 
+});
 
 
 
