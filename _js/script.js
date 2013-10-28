621b9be9421f57ac4bd79be2d11fcc15e1b07241
Boneyard <Boneyard@Boneyards-MacBook-Pro.local>
2013-10-28 16:45:11 -0400

    set up script to filter hoops by location

--- _js/script.js
+++ _js/script.js
@@ -21,11 +21,35 @@ $(window).scroll(function() {
     }, 25);
 });
 
-//Mobile nav toggling action-action-action
+    //Mobile nav toggling action-action-action
 	$(".m-nav-toggle").click(function(){
 	  	console.log('clicked');
 	    $("#nav-link-mod").toggleClass("visible");
 	});
+
+    //detect click on a location - then filter results based on specific location
+    $(".hood-list-item a").click(function(e) {
+        e.preventDefault();
+        //Remove the active state from whichever button is active
+       $(".hood-list-item a.active").removeClass('active');
+        //And add the active state to whichever button was clicked
+        $(this).addClass('active');
+
+       var location = $(this).data("location");
+       //check whether it equals all
+       if (location == "all") {
+           $(".stream-item").show();
+       }
+       else {
+           $(".stream-item").hide();
+           $("."+location).show()       
+        }
+    });
+
+        //when viewport reaches about section ball (set up alert box when position reached to test) (testing for position of element relative to viewport) (set up console.log to see what position is)
+
+
+        //becomes position absolute
 
 
 });
