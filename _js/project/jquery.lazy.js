ce4d8b0631fe01cd4b6a57f857ca5468425da2c9
Boneyard <Boneyard@Boneyards-iMac.local>
2014-06-30 15:04:19 -0400

    Updating TypeKit and synching with online site

--- _js/project/jquery.lazy.js
+++ _js/project/jquery.lazy.js
@@ -29,7 +29,7 @@
             skip_invisible  : true,
             appear          : null,
             load            : null,
-            placeholder     : "data:_img/blank.gif"
+            placeholder     : "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAANSURBVBhXYzh8+PB/AAffA0nNPuCLAAAAAElFTkSuQmCC"
         };
 
         function update() {
